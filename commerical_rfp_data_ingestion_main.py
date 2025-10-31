904b38853e524fb28efaf1e301e64d6e717b0655
Dharma raja B L <87357702+Dharmaraj777@users.noreply.github.com>
2025-10-31 08:39:53 -0400

    Update commerical_rfp_data_ingestion_main.py

--- commerical_rfp_data_ingestion_main.py
+++ commerical_rfp_data_ingestion_main.py
@@ -1,1 +1,29 @@
+import os
+import json
+from datetime import datetime
+import pandas as pd
+from commercial_rfp_shared_logger import logger
+from commercial_rfp_raw_data_ingestion_and_cleaning import DataIngestion
+import time
 
+def main():
+    today_date = datetime.now().strftime("%Y-%m-%d")
+    log_file_name = f"commercial_rfp_data_processor_{today_date}.log"
+    config_file='config.json'
+    
+    with open(config_file, 'r') as f:
+        config_details = json.load(f)
+
+    
+    #Define all variable here. Remove this logic from the individual files
+    data_ingestion = DataIngestion()
+    doc_creation = ''
+    logger.info("******************************************************")
+    data_ingestion.commercial_rfp_data_cleaning()
+    time.sleep(10)
+
+    doc_creation#.commercial_rfp_data_cleaning()
+    time.sleep(10)
+
+if __name__ == "__main__":
+    main()
