88e6f1b2c9ca8907ed73eaa5ace21aeb5a06a9b8
Dharma raja B L <87357702+Dharmaraj777@users.noreply.github.com>
2025-11-04 08:22:09 -0500

    Update commerical_rfp_data_ingestion_main.py

--- commerical_rfp_data_ingestion_main.py
+++ commerical_rfp_data_ingestion_main.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from commercial_rfp_shared_logger import logger
 from commercial_rfp_config_loader import ConfigLoader
+from commercial_rfp_create_blob_containers import CreatContainers
 from commercial_rfp_data_ingestion_utils import UtilityFunctions
 from commercial_rfp_raw_data_ingestion_and_cleaning import DataIngestion
 from commercial_rfp_content_doc_library_creation import DocLibraryCreator
@@ -15,6 +16,9 @@ def main():
     logger.info("*********** Commercial RFP Data Pipeline: START ***********")
 
     try:
+        container_creator = CreatContainers()
+        container_creator.creat_containers()
+
         data_ingestion = DataIngestion()
         data_ingestion.commercial_rfp_data_cleaning()
 
@@ -27,14 +31,10 @@ def main():
         citation_mapper = CitationMapper()
         citation_mapper.upload_citation_files_to_sharepoint()
 
-        utils.upload_log_to_blob(
-            log_file_name, config_loader.commercial_rfp_logs, config_loader.blob_service_client
-        )
+        utils.upload_log_to_blob(log_file_name, config_loader.commercial_rfp_logs, config_loader.blob_service_client)
     except Exception as e:
         logger.error(f"An error occurred in the main process: {e}")
-        utils.upload_log_to_blob(
-            log_file_name, config_loader.commercial_rfp_logs, config_loader.blob_service_client
-        )
+        utils.upload_log_to_blob(log_file_name, config_loader.commercial_rfp_logs, config_loader.blob_service_client)
 
     logger.info("*********** END of Processing File ***********")
 
