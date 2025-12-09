8fb77e9a6613b51e2b8ca12e862c315520a740c2
Dharma raja B L <87357702+Dharmaraj777@users.noreply.github.com>
2025-12-09 06:05:55 -0500

    Update fix.py

--- 1/fix.py
+++ 1/fix.py
@@ -1,4 +1,33 @@
-2025-11-03 07:48:44,773 - INFO - *********** Commercial RFP Data Pipeline: START ***********
-2025-11-03 07:48:44,774 - ERROR - An error occurred in the main process: bad operand type for unary -: 'str'
-2025-11-03 07:48:46,110 - INFO - Log file commercial_rfp_data_processor_2025-11-03.log uploaded successfully to 'commercial-rfp-logs'.
-2025-11-03 07:48:46,111 - INFO - *********** END of Processing File ***********
+import pandas as pd
+import hashlib
+
+
+def key_from_hash(question, algo):
+    snippet = question[:120]  # or use the full question, depending on preference
+    data = snippet.encode('utf-8')
+    if algo == 'md5':
+        hash_hex = hashlib.md5(data).hexdigest()
+    elif algo == 'sha1':
+        hash_hex = hashlib.sha1(data).hexdigest()
+    elif algo == 'sha256':
+        hash_hex = hashlib.sha256(data).hexdigest()
+    else:
+        raise ValueError("Unsupported hash algorithm")
+    return 'RFP_Content_'+hash_hex
+
+
+def generate_rfp_key(df):
+    df['Date'] = pd.to_datetime(df['Date'], errors='coerce').dt.strftime('%Y-%m-%d')
+    df['key'] = df.apply(lambda row: f"{str(row['Client Name']).strip()}_{str(row['Date']).strip()}_{str(row['RFP Type']).strip()}_{str(row['Question']).strip()}", axis=1)
+    df['key_hash'] = df['key'].str.replace(' ','').apply(lambda x: key_from_hash(x, algo='md5'))
+    return df
+
+
+
+filepath = r"C:\Users\ODKPTSO\Downloads\RFP Content June 2025.xlsx"
+df = pd.read_excel(filepath)
+
+df=df[['Client Name',	'RFP Type',	'Consultant',	'Date',	'Question',	'Response']]
+df = generate_rfp_key(df)
+
+df.head(2)
